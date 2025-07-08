def661f41390e40b63570e70d08346788aea46b5
HEMANRONNEN <rommelchalls8@gmail.com>
2025-07-08 16:15:08 +0530

    class1 of c

--- c1.c
+++ c1.c
@@ -1,8 +1,45 @@
 #include<stdio.h>
+#include<conio.h>
 void main(){
 
-    int a;
-    printf("hello world put integer \n");
-    scanf("%d",&a);
-    printf("this is what you have choosen : %d",a);
+    float a,b;
+   /* printf("input 2 integer \t");
+    scanf("%d\t %d",&a,&b);
+    printf("this is your sum : %d \n",a+b);*/
+
+   /* printf("input length and breath \t");
+    scanf("%d \t %d",&a,&b);
+    printf("this is your area : %d",a*b);    
+    printf("\nthis is your perimeter : %d",2*(a*b));*/
+
+   /* printf("enter the integers you want to swap(decimals are accepted)\t");
+    scanf("%f \t %f",&a,&b);
+    float z=a;
+    a=b;
+    b=z;
+    printf(" your swap variables \n  a = %.2f \n b = %.2f",a,b);*/
+
+
+
+    /*printf("\nenter values for  P : ");
+    scanf("%f",&a);
+
+    printf("\nenter value for R : ");
+    scanf("%f",&b);
+
+    float c;
+    printf("\nenter values for  T : ");
+    scanf("%f",&c);
+
+    float si=(a*b*c)/100;
+    printf("your SI : %f",si);*/
+
+
+
+    printf("enter your values for x and y \t");
+    scanf("%f %f",&a,&b);
+    float exp=((a+b)*(a-b));
+    printf("\nyour value of expression is : %f",exp);
+
+
 }
